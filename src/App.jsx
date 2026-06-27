1c15c6b7502d3c45d28d34a29ff853e467d1c2a8
Arpan Shukla <devarpan23@gmail.com>
2026-06-27 06:55:00 +0530

    chore: initialize React application using Vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import CommentComposer from './components/CommentComposer/CommentComposer'
+
+
+function App() {
+
+
+  return (
+    <>
+    <CommentComposer/>
+
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CommentComposer/CommentComposer.jsx
@@ -0,0 +1,7 @@
+export default function CommentComposer(){
+    return(
+        <>
+        <h1>Social Post Composer</h1>
+        </>
+    )
+}

db8e2a8ec7a25966981838b9d5d3fa0f71da913b
Arpan Shukla <devarpan23@gmail.com>
2026-06-27 20:15:24 +0530

    feat: compose comment composer UI

--- src/components/CommentComposer/CommentComposer.jsx
+++ src/components/CommentComposer/CommentComposer.jsx
@@ -1,7 +1,23 @@
+// src/components/CommentComposer/CommentComposer.jsx
+import Avatar from "./ComposerBody/Avatar.jsx";
+import TextArea from "./ComposerBody/TextArea.jsx";
+import CharacterCounter from "./ComposerFooter/CharacterCounter";
+import SubmitButton from "./ComposerFooter/SubmitButton";
+import "./CommentComposer.css";
+
 export default function CommentComposer(){
     return(
-        <>
-        <h1>Social Post Composer</h1>
-        </>
+        <form 
+            className="comment-composer"
+            onSubmit={handleSubmit}>
+            <div className="composer-body">
+                <Avatar/>
+                <TextArea/>
+            </div>
+            <div className="composer-footer">
+                <CharacterCounter/>
+                <SubmitButton/>
+            </div>
+        </form>
     )
 }
